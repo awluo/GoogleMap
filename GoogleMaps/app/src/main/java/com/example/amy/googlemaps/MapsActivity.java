d0ec643abe8bd57f6093e2f65535082b3910f466
luoa2945 <AmyLuo1999@gmail.com>
2017-05-30 11:05:59 -0700

    Added Search and Track Buttons. Set up getLocation method and Location Listener.

--- GoogleMaps/app/src/main/java/com/example/amy/googlemaps/MapsActivity.java
+++ GoogleMaps/app/src/main/java/com/example/amy/googlemaps/MapsActivity.java
@@ -2,12 +2,16 @@ package com.example.amy.googlemaps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,6 +23,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private boolean isGPSenabled = false;
+    private boolean isNetworkEnabled = false;
+    private boolean canGetLocation = false;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 15;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 5.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,23 +62,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sanfran).title("Born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sanfran));
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.d("MyMappsApp", "Failed Permission check 1");
             Log.d("MyMappsApp", Integer.toString(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)));
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
         }
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
             Log.d("MyMapsApp", "Failed Permission check 2");
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
         }
         mMap.setMyLocationEnabled(true);
     }
 
-    public void setView(View v){
-        if(mMap.getMapType()== GoogleMap.MAP_TYPE_SATELLITE)
+    public void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            //get GPS status
+            isGPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if (isGPSenabled) Log.d("MyMaps", "getLocation: GPS is enabled");
+
+            //get network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (isNetworkEnabled) Log.d("MyMaps", "getLocation: Network is enabled");
+
+            if (!isGPSenabled && !isNetworkEnabled) {
+                Log.d("MyMaps", "getLocation: No Provider is enabled");
+            } else {
+
+                canGetLocation = true;
+                if(isGPSenabled){
+                    Log.d("MyMaps", "getLocation: GPS Enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerGps);
+                    Log.d("MyMaps", "getLocation: GPS update request success");
+                    Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
+                }
+                if(isNetworkEnabled){
+                    Log.d("MyMaps", "getLocation: Network Enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerNetwork);
+                    Log.d("MyMaps", "getLocation: Network update request success");
+                    Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
+                }
+            }
+
+        } catch (Exception e) {
+            Log.d("MyMaps", "getLocation: Caught an exeption in getLocation");
+            e.printStackTrace();
+        }
+    }
+
+    public void setView(View v) {
+        if (mMap.getMapType() == GoogleMap.MAP_TYPE_SATELLITE)
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         else
             mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+
+    public class locationListenerGps implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            location.getLatitude();
+            location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText( getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText( getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT ).show();
+        }
     }
 }
