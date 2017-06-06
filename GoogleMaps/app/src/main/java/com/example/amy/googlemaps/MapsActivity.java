a92a091b31ec51931b57cdab970fa4ce1854bad9
luoa2945 <AmyLuo1999@gmail.com>
2017-06-05 21:23:42 -0700

    Added onSearch method and clearMarkers.

--- GoogleMaps/app/src/main/java/com/example/amy/googlemaps/MapsActivity.java
+++ GoogleMaps/app/src/main/java/com/example/amy/googlemaps/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.amy.googlemaps;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,6 +15,7 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -27,6 +30,9 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -307,5 +313,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         @Override
         public void onProviderDisabled(String proFvider) {}
     };
+
+    public void onSearch(View v)
+    {
+        EditText location_text = (EditText)findViewById(R.id.Search_Text);
+        String location = location_text.getText().toString();
+        List<Address> addressList = null;
+        if (location != null || ! location.equals(""))
+        {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, MY_LOC_ZOOM_FACTOR);
+            mMap.animateCamera(update);
+        }
+        else
+        {
+            Log.d("MyMapsApp", "getSearch: empty Search Text");
+            Toast.makeText(this, "No location entered", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void ClearMarkers(View v)
+    {
+    }
 
 }
